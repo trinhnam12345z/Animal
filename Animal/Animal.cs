e06de46eec54ba550870abc3b829d219afc361e6
trinhnam12345z <trinhnam123456789z@gmail.com>
2020-09-22 09:37:57 +0700

    Initial commit

---
+++ Animal/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animal
+{
+    public class Animal
+    {
+        public string name { get; set; }
+        public string sound { get; set; }
+        public int idNum { get; }
+        public static int numOfAnimal = 1;
+        public Animal()
+        {
+
+        }
+        public Animal(string name, string sound, int idNum)
+        {
+            this.name = name;
+            this.sound = sound;
+            this.idNum = idNum;
+        }
+        public int numofAnimal()
+        {
+            return numOfAnimal++;
+        }
+        public string MakeSound()
+        {
+            return name + sound;
+        }
+
+    }
+}
---
+++ Animal/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int d = 0;
+            Animal a = new Animal();
+            while (d >= 0)
+            {
+                d++;
+                Console.WriteLine("Nhap ten dong vat: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Nhap tieng keu dong vat: ");
+                string sound = Console.ReadLine();
+                Console.WriteLine(a.idNum);
+                Console.WriteLine(name + " se keu " + sound);
+                Console.WriteLine(a.numofAnimal());
+            }
+        }
+    }
+    
+}
